57770b8e799d4e1da6395ccb9b92cfd1a3ba6f0b
1000ch <shogo.sensui@gmail.com>
2014-02-27 18:52:22 +0900

    fix

--- gruntfile.coffee
+++ gruntfile.coffee
@@ -4,11 +4,6 @@ module.exports = (grunt) ->
     ystkyrk:
       the_101st_propose:
         file: './sounds/the_101st_propose.mp3'
-    version:
-      options:
-        base: 'base.json'
-      files: 'package.json'
   
   grunt.loadTasks 'tasks'
-  grunt.loadNpmTasks 'grunt-sync-version'
   grunt.registerTask 'default', ['ystkyrk:the_101st_propose']
--- tasks/ystkyrk.js
+++ tasks/ystkyrk.js
@@ -4,8 +4,8 @@ module.exports = function (grunt) {
   var play = require('play');
 
   grunt.registerMultiTask('ystkyrk', 'the 101st propose.', function () {
-    if (grunt.file.exists(this.data.file)) {
-      grunt.log.error('she is not here.');
+    if (!grunt.file.exists(this.data.file)) {
+      grunt.log.error('She is not here.');
       return;
     }
     play.sound(this.data.file);
